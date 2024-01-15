08ae976b4f7f894d919aafd3e7bde2138957bb47
Tatyane Gonçalves <tatygoncalves97@gmail.com>
2024-01-15 11:28:55 -0300

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form")
+const card1 = document.querySelector(".card")
+const card2 = document.querySelector(".card-2")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    card1.classList.add("hide");
+    card2.classList.remove("hide");
+})
